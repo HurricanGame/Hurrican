d7760ff9ad143fd9e6f55381219d01d743d7c74b
pickle136 <pickle136@a024ca3c-242f-4dd3-966e-ca4e61bf0f28>
2015-02-02 15:23:29 +0000

    Update copyright years

--- trunk/Hurrican/src/SDLPort/texture.h
+++ trunk/Hurrican/src/SDLPort/texture.h
@@ -1,6 +1,6 @@
 /**
  *
- *  Copyright (C) 2011-2012 Scott R. Smith
+ *  Copyright (C) 2011-2015 Scott R. Smith
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
