d16e33148146217d19dac5587c85e9443c9417d9
senquack <senquack@a024ca3c-242f-4dd3-966e-ca4e61bf0f28>
2015-02-14 06:52:05 +0000

    Removed unneeded PLATFORM_GL #define from previous commit

--- Hurrican/src/DX8Graphics.h
+++ Hurrican/src/DX8Graphics.h
@@ -18,7 +18,6 @@
 // --------------------------------------------------------------------------------------
 
 #if defined(PLATFORM_SDL)
-#define PLATFORM_GL
 #include "SDL_port.h"
 #include "texture.h"
 #if defined(USE_GL2)
