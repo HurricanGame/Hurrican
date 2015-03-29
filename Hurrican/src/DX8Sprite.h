08195cf1ae3178d0928bac63f9df1a2b1fbc2152
senquack <senquack@a024ca3c-242f-4dd3-966e-ca4e61bf0f28>
2015-03-29 19:25:57 +0000

    Optimizations to DX8Sprite.cpp and its header file: Converted some ints to shorts. Moved some data members to top of class since they were used most often. Disabled some never-used functions. Sprite class member itsPreCalcedRects is no longer a needlessly huge array, but a dynamically allocated one. Made many functions in DX8Sprite that stupidly returned an unused bool return void instead. Many very small functions are now inlined header functions. Disabled the one use of the RenderCircle() function in a debug block in Trigger_FadeMusic.cpp, allowing its disabling in DX8Sprite.cpp. Removed never-used SetRect() and GetRect() in DirectGraphicsSurface and DirectGraphicsSprite classes. Inlined constructor in sprite class in DX8Sprite.cpp. Added QUAD2D definition in DX8Graphics.h. Converted Projectiles.cpp draw functions to use local TriangleStrips instead of globals declared in DX8Sprite.cpp. Converted Partikelsystem.cpp draw functions to use local TriangleStrips instead of globals declared in DX8Sprite.cpp.

--- Hurrican/src/DX8Sprite.h
+++ Hurrican/src/DX8Sprite.h
@@ -40,13 +40,6 @@
 // --------------------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------------------
-// Globale Spritefunktionen ausserhalb der Klassen
-// --------------------------------------------------------------------------------------
-
-void SetClippingArea(int Left, int Top, int Right, int Bottom);	// Neue ClippingArea setzen
-RECT GetClippingArea(void);										// Aktuelle Clipping Area abfragen
-
-// --------------------------------------------------------------------------------------
 // Klassendeklaration
 // --------------------------------------------------------------------------------------
 
@@ -66,9 +59,10 @@ public:
     ~DirectGraphicsSurface(void);						// Surface freigeben
     bool LoadImage(const char *Filename,					// Laden des Bildes "Filename"
                    int xSize, int ySize);					// mit Grösse xSize, ySize
-    bool  SetRect	  (int left,  int top,
-                       int right, int bottom);				// Neuen Ausschnitt setzen
-    RECT  GetRect	  (void);								// Ausschnitt holen
+    //DKS - these are never used anywhere, disabled:
+    //bool  SetRect	  (int left,  int top,
+    //                   int right, int bottom);				// Neuen Ausschnitt setzen
+    //RECT  GetRect	  (void);								// Ausschnitt holen
 #if defined(PLATFORM_DIRECTX)
     bool  DrawSurface (LPDIRECT3DSURFACE8 &Temp,			// Bild auf Surface Temp anzeigen
                        int xPos, int yPos);					// an Position xPos/yPos
@@ -83,76 +77,147 @@ public:
 
 class DirectGraphicsSprite
 {
+public:
+    //DKS - Moved itsRect to the top, since it is used most-frequently:
+    RECT				itsRect;							// zu zeigender Ausschnitt
+
 private:
-    int					itsXFrameCount;						// Frameanzahl in X-Richtung
-    int					itsYFrameCount;						// Frameanzahl in Y-Richtung
+    //DKS - Made these shorts instead of ints:
+    int16_t				itsXFrameCount;						// Frameanzahl in X-Richtung
+    int16_t				itsYFrameCount;						// Frameanzahl in Y-Richtung
 
 public:
-    RECT				itsRect;							// zu zeigender Ausschnitt
     float				itsXSize;							// x-Grösse der Textur
     float				itsYSize;							// y-Grösse der Textur
-    int					itsXFrameSize;						// Framegrösse in X-Richtung
-    int					itsYFrameSize;						// Framegrösse in Y-Richtung
+    //DKS - Made these shorts instead of ints:
+    int16_t				itsXFrameSize;						// Framegrösse in X-Richtung
+    int16_t				itsYFrameSize;						// Framegrösse in Y-Richtung
 
 
-    RECT				itsPreCalcedRects[256];				// vorberechnete Ausschnitte für die Frames
+    //DKS - these are now dynamically allocated:
+    //RECT				itsPreCalcedRects[256];				// vorberechnete Ausschnitte für die Frames
+    RECT				*itsPreCalcedRects;				// vorberechnete Ausschnitte für die Frames
+
     LPDIRECT3DTEXTURE8	itsTexture;							// Textur mit Grafikdaten
-    DirectGraphicsSprite(void);						// Konstruktor (leer)
+
+    //DKS - inlined
+    DirectGraphicsSprite(void)
+    {
+#if defined(PLATFORM_DIRECTX)
+        itsTexture = (LPDIRECT3DTEXTURE8)NULL;
+#elif defined(PLATFORM_SDL)
+        itsTexture = -1;
+#endif
+        itsPreCalcedRects = NULL;
+    }
+
     ~DirectGraphicsSprite(void);						// Textur freigeben
 
     bool  LoadImage(const char *Filename,							// Laden des Bildes "Filename"
                     int xs, int ys, int xfs, int yfs,		// x/y Size und Framegrösse
                     int xfc,  int yfc);						// Frameanzahl
-    bool  SetRect	  (int left,  int top,
-                       int right, int bottom);				// Neuen Ausschnitt setzen
-    RECT  GetRect	  (void);								// Ausschnitt holen
-    bool  RenderSprite(float x, float y, D3DCOLOR Color);	// Sprite rendern
-    bool  RenderSprite(float x, float y, int Anim,
+
+    //DKS - Made inline
+    void SetRect	  (int left,  int top,
+                       int right, int bottom)				// Neuen Ausschnitt setzen
+    {
+        itsRect.left   = left;										// Linker  Rand
+        itsRect.right  = right;										// Rechter Rand
+        itsRect.top    = top;										// Oberer  Rand
+        itsRect.bottom = bottom;									// Unterer Rand
+    }
+    //DKS - this is never used anywhere, disabled:
+    //RECT  GetRect	  (void);								// Ausschnitt holen
+
+    //DKS - All of the following rendering functions no longer pointlessly return bool value of true:
+    void  RenderSprite(float x, float y, D3DCOLOR Color);	// Sprite rendern
+
+    void  RenderSprite(float x, float y, int Anim,
                        D3DCOLOR c1,
                        D3DCOLOR c2,
                        D3DCOLOR c3,
                        D3DCOLOR c4);		// Sprite rendern mit vier farbigen Eckpunkten
-    bool  RenderMirroredSprite(float x, float y,			// Sprite spiegelverkehrt rendern
+
+    void  RenderMirroredSprite(float x, float y,			// Sprite spiegelverkehrt rendern
                                D3DCOLOR Color);
-    bool  RenderMirroredSprite(float x, float y,			// Sprite Vertikal spiegelverkehrt rendern
+    void  RenderMirroredSprite(float x, float y,			// Sprite Vertikal spiegelverkehrt rendern
                                D3DCOLOR Color, bool h, bool v);
-    bool  RenderSprite(float x, float y, int Anim,			// Sprite rendern mit Animation
-                       D3DCOLOR Color);
-    bool  RenderSprite(float x, float y, int Anim,			// Sprite rendern mit Animation, wahlweise
-                       D3DCOLOR Color, bool mirrored);		// spiegelverkehrt
-    bool  RenderSpriteScaled(float x, float y,				// Sprite in beliebiger Grösse
+
+    //DKS - Inlined:
+    void RenderSprite(float x, float y, int Anim, D3DCOLOR Color)
+    {
+        // Ausschnitt einstellen
+        //DKS - TODO: can this be changed to a '& 0xFF'?
+        Anim %= 255;
+        itsRect = itsPreCalcedRects [Anim];
+
+        // Und Sprite rendern
+        RenderSprite(x, y, Color);
+    }
+
+    //DKS - Inlined:
+    void  RenderSprite(float x, float y, int Anim, D3DCOLOR Color, bool mirrored)
+    {
+        // Ausschnitt einstellen
+        //DKS - can we change this to '& 0xFF'?
+        Anim %= 255;
+        itsRect = itsPreCalcedRects [Anim];
+
+        // Und Sprite rendern
+        if (mirrored == false)
+            RenderSprite(x, y, Color);
+        else
+            RenderMirroredSprite(x, y, Color);
+    }
+
+    void  RenderSpriteScaled(float x, float y,				// Sprite in beliebiger Grösse
                              int width, int height,
                              D3DCOLOR col);
-    bool  RenderSpriteScaled(float x, float y,				// Sprite in beliebiger Grösse mit Animationsphase
+
+    void  RenderSpriteScaled(float x, float y,				// Sprite in beliebiger Grösse mit Animationsphase
                              int width, int height,
                              int Anim,
                              D3DCOLOR col);
-    bool  RenderSpriteRotated(float x, float y, float Winkel,// Sprite rotiert darstellen
+    
+    void  RenderSpriteRotated(float x, float y, float Winkel,// Sprite rotiert darstellen
                               D3DCOLOR Color);
-    bool  RenderSpriteRotated(float x, float y, float Winkel,// Sprite rotiert mit animphase darstellen
-                              int Anim, D3DCOLOR Color);
-    bool  RenderSpriteRotated(float x, float y, float Winkel,// Sprite rotiert mit animphase gespiegelt darstellen
+
+    void  RenderSpriteRotated(float x, float y, float Winkel,// Sprite rotiert mit animphase gespiegelt darstellen
                               int Anim, D3DCOLOR Color, bool mirror);
-    bool  RenderSpriteScaledRotated(float x, float y,		// beliebige Grösse und rotiert
+
+    //DKS - Inlined:
+    void RenderSpriteRotated(float x, float y, float Winkel,// Sprite rotiert mit animphase darstellen
+            int Anim, D3DCOLOR Color)
+    {
+        // Ausschnitt einstellen
+        Anim %= 255;
+        itsRect = itsPreCalcedRects [Anim];
+
+        // Und Sprite rendern
+        RenderSpriteScaledRotated (x, y,
+                float (itsRect.right  - itsRect.left),
+                float (itsRect.bottom - itsRect.top), Winkel, Color);
+    }
+
+    void  RenderSpriteScaledRotated(float x, float y,		// beliebige Grösse und rotiert
                                     float width, float height,
                                     float Winkel, D3DCOLOR Color);
-    bool  RenderSpriteRotatedOffset(float x, float y, float Winkel,	// Sprite rotiert darstellen mit Verschiebung
+
+    void  RenderSpriteRotatedOffset(float x, float y, float Winkel,	// Sprite rotiert darstellen mit Verschiebung
                                     float offx, float offy, D3DCOLOR Color, bool mirrored = false);
 
-    float GetXSize(void);										// Membervariablen holen
-    float GetYSize(void);
-    int   GetXFrameSize(void);
-    int   GetYFrameSize(void);
-    int   GetXFrameCount(void);
-    int   GetYFrameCount(void);
+    //DKS - inlined these:
+    float GetXSize(void)        { return itsXSize; }
+    float GetYSize(void)        { return itsYSize; }
+    int   GetXFrameSize(void)   { return itsXFrameSize; }
+    int   GetYFrameSize(void)   { return itsYFrameSize; }
+    int   GetXFrameCount(void)  { return itsXFrameCount; }
+    int   GetYFrameCount(void)  { return itsYFrameCount; }
 };
 
 // --------------------------------------------------------------------------------------
 // Prototypen von sonstigen Funktionen
 // --------------------------------------------------------------------------------------
-
-void  RenderRect(float x, float y, float width,									// Rechteck anzeigen
-                 float height, D3DCOLOR Color);
 
 void  RenderRect4(float x, float y, float width,									// Rechteck anzeigen mit 4 farbigen Ecken
                   float height,
@@ -161,23 +226,41 @@ void  RenderRect4(float x, float y, float width,									// Rechteck anzeigen mi
                   D3DCOLOR c3,
                   D3DCOLOR c4);
 
+//DKS - Inlined:
+static inline void RenderRect(float x, float y, float width, float height, D3DCOLOR Color)
+{
+	RenderRect4(x, y , width, height, Color, Color, Color, Color);
+}
+
 void  RenderLine(D3DXVECTOR2 p1, D3DXVECTOR2 p2,								// Linie rendern
                  D3DCOLOR Color);
 void RenderLine (D3DXVECTOR2 p1, D3DXVECTOR2 p2,								// Linie rendern (zwei farbig)
                  D3DCOLOR Color1, D3DCOLOR Color2);
 
-void RenderCircle(float x, float y, float r, D3DCOLOR col);
+//DKS - Disabled, as it is no longer used anywhere:
+//void RenderCircle(float x, float y, float r, D3DCOLOR col);
 
 
-bool  SpriteCollision(float x1, float y1, RECT rect1,							// Kollision zwischen zwei Sprites
-                      float x2, float y2, RECT rect2);
+//DKS - Inlined this, since it is simple and used extremely frequently and takes many parameters:
+//bool  SpriteCollision(float x1, float y1, RECT rect1,							// Kollision zwischen zwei Sprites
+//                      float x2, float y2, RECT rect2);
+static inline bool  SpriteCollision(float x1, float y1, RECT &rect1,
+                                    float x2, float y2, RECT &rect2)
+{
+    // Kollision der Bounding-Boxes ?
+    if (x1 + rect1.left < x2 + rect2.right   &&
+        x2 + rect2.left < x1 + rect1.right   &&
+        y1 + rect1.top  < y2 + rect2.bottom  &&
+        y2 + rect2.top  < y1 + rect1.bottom)
+        return true;
+    else
+        return false;
+}
 
 // --------------------------------------------------------------------------------------
 // Externals
 // --------------------------------------------------------------------------------------
 
-extern VERTEX2D				v1, v2, v3, v4;						// Vertices zum Schuss rendern
-extern VERTEX2D				TriangleStrip[4];					// Strip für einen Schuss
 extern DirectGraphicsSprite LoadingScreen;
 extern DirectGraphicsSprite LoadingBar;
 extern float				LoadingProgress;
