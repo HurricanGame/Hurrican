3b37d98c0eda2808bc456e75246991911303a016
Stefan Schmidt <thrimbor.github@gmail.com>
2018-04-03 20:12:01 +0200

    Switched to libepoxy

---
+++ Hurrican/3rdparty/FindLibEpoxy.cmake
@@ -0,0 +1,29 @@
+# - Try to find LibEpoxy
+# Once done, this will define
+#
+#  LibEpoxy_FOUND - system has LibEpoxy
+#  LibEpoxy_INCLUDE_DIRS - the LibEpoxy include directories
+#  LibEpoxy_LIBRARIES - link these to use LibEpoxy
+#
+# See documentation on how to write CMake scripts at
+# http://www.cmake.org/Wiki/CMake:How_To_Find_Libraries
+
+include(LibFindMacros)
+
+libfind_pkg_check_modules(LibEpoxy_PKGCONF epoxy)
+
+find_path(LibEpoxy_INCLUDE_DIR
+  NAMES epoxy/gl.h
+  HINTS ${LibEpoxy_PKGCONF_INCLUDE_DIRS}
+  PATH_SUFFIXES LibEpoxy
+)
+
+find_library(LibEpoxy_LIBRARY
+  NAMES epoxy
+  HINTS ${LibEpoxy_PKGCONF_LIBRARY_DIRS}
+)
+
+set(LibEpoxy_PROCESS_INCLUDES LibEpoxy_INCLUDE_DIR)
+set(LibEpoxy_PROCESS_LIBS LibEpoxy_LIBRARY)
+set(LibEpoxy_VERSION ${LibEpoxy_PKGCONF_VERSION})
+libfind_process(LibEpoxy)
--- Hurrican/src/SDLPort/opengl.hpp
+++ Hurrican/src/SDLPort/opengl.hpp
@@ -1,6 +1,9 @@
 #ifndef _OPENGL_H_
 #define _OPENGL_H_
 
+#include <epoxy/gl.h>
+#include <epoxy/glx.h>
+#if 0
 #include "SDL.h"
 
 // Do some sanity checks
@@ -12,10 +15,6 @@
 #error "ERROR USE_GL1 and USE_GLES2 are defined. Replace USE_GLES2 for USE_GLES1"
 #elif defined(USE_GL2) && defined(USE_GLES1)
 #error "ERROR USE_GL2 and USE_GLES1 are defined. Replace USE_GLES1 for USE_GLES2"
-#endif
-
-#if !defined(__WIN32)
-#define GL_GLEXT_PROTOTYPES 1
 #endif
 
 #if SDL_VERSION_ATLEAST(2, 0, 0)
@@ -43,7 +42,7 @@
 #endif
 
 #else /* Full OpenGL Profile */
-#include "SDL_opengl.h"
+//#include "SDL_opengl.h"
 #endif
 
 #endif
@@ -52,72 +51,5 @@
 #define glClearDepth glClearDepthf
 #endif
 
-#ifndef GL_GLEXT_PROTOTYPES
-#if defined(__WIN32__)
-#if defined(USE_GL2)
-/* OpenGL Version 2.0 API */
-extern PFNGLDELETESHADERPROC glDeleteShader;
-extern PFNGLDELETEPROGRAMPROC glDeleteProgram;
-extern PFNGLUSEPROGRAMPROC glUseProgram;
-extern PFNGLCREATESHADERPROC glCreateShader;
-extern PFNGLSHADERSOURCEPROC glShaderSource;
-extern PFNGLCOMPILESHADERPROC glCompileShader;
-extern PFNGLGETSHADERIVPROC glGetShaderiv;
-extern PFNGLCREATEPROGRAMPROC glCreateProgram;
-extern PFNGLATTACHSHADERPROC glAttachShader;
-extern PFNGLLINKPROGRAMPROC glLinkProgram;
-extern PFNGLGETPROGRAMIVPROC glGetProgramiv;
-extern PFNGLGETACTIVEATTRIBPROC glGetActiveAttrib;
-extern PFNGLGETATTRIBLOCATIONPROC glGetAttribLocation;
-extern PFNGLGETACTIVEUNIFORMPROC glGetActiveUniform;
-extern PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation;
-extern PFNGLGETSHADERINFOLOGPROC glGetShaderInfoLog;
-extern PFNGLGETPROGRAMINFOLOGPROC glGetProgramInfoLog;
-extern PFNGLDISABLEVERTEXATTRIBARRAYPROC glDisableVertexAttribArray;
-extern PFNGLENABLEVERTEXATTRIBARRAYPROC glEnableVertexAttribArray;
-extern PFNGLVERTEXATTRIBPOINTERPROC glVertexAttribPointer;
-extern PFNGLUNIFORM1IPROC glUniform1i;
-extern PFNGLUNIFORM1FVPROC glUniform1fv;
-extern PFNGLUNIFORM3FVPROC glUniform3fv;
-extern PFNGLUNIFORM4FVPROC glUniform4fv;
-extern PFNGLUNIFORMMATRIX3FVPROC glUniformMatrix3fv;
-extern PFNGLUNIFORMMATRIX4FVPROC glUniformMatrix4fv;
-/* FBO API */
-#if defined(USE_FBO)
-#define GL_FRAMEBUFFER GL_FRAMEBUFFER_EXT
-#define GL_FRAMEBUFFER_COMPLETE GL_FRAMEBUFFER_COMPLETE_EXT
-#define GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT_EXT
-#define GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS_EXT
-#define GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT_EXT
-#define GL_FRAMEBUFFER_UNSUPPORTED GL_FRAMEBUFFER_UNSUPPORTED_EXT
-#define GL_COLOR_ATTACHMENT0 GL_COLOR_ATTACHMENT0_EXT
-#define glBindFramebuffer glBindFramebufferEXT
-#define glDeleteFramebuffers glDeleteFramebuffersEXT
-#define glGenFramebuffers glGenFramebuffersEXT
-#define glCheckFramebufferStatus glCheckFramebufferStatusEXT
-#define glFramebufferTexture2D glFramebufferTexture2DEXT
-#define glGenRenderbuffers glGenRenderbuffersEXT
-#define glBindRenderbuffer glBindRenderbufferEXT
-#define glRenderbufferStorage glRenderbufferStorageEXT
-#define glDeleteRenderbuffers glDeleteRenderbuffersEXT
-extern PFNGLBINDFRAMEBUFFEREXTPROC glBindFramebuffer;
-extern PFNGLDELETEFRAMEBUFFERSEXTPROC glDeleteFramebuffers;
-extern PFNGLGENFRAMEBUFFERSEXTPROC glGenFramebuffers;
-extern PFNGLCHECKFRAMEBUFFERSTATUSEXTPROC glCheckFramebufferStatus;
-extern PFNGLFRAMEBUFFERTEXTURE2DEXTPROC glFramebufferTexture2D;
-extern PFNGLGENRENDERBUFFERSEXTPROC glGenRenderbuffers;
-extern PFNGLBINDRENDERBUFFEREXTPROC glBindRenderbuffer;
-extern PFNGLRENDERBUFFERSTORAGEEXTPROC glRenderbufferStorage;
-extern PFNGLDELETERENDERBUFFERSEXTPROC glDeleteRenderbuffers;
-#endif /* defined(USE_FBO) */
 #endif
-
-#if defined(USE_GL1) || defined(USE_GL2)
-/* GL_ARB_texture_compression */
-extern PFNGLCOMPRESSEDTEXIMAGE2DARBPROC glCompressedTexImage2D;
-extern PFNGLGETCOMPRESSEDTEXIMAGEARBPROC glGetCompressedTexImageARB;
-#endif
-#endif /* WIN32 */
-#endif
-
 #endif /* _OPENGL_H_ */
